3eb46a619318236dc266b9877c6bc7c1c35698df
Shoby <shobikaksr@gmail.com>
2015-03-08 22:33:53 -0400

    Project With Search Control and Feature

--- app/src/main/java/proj/wifilocator/MainActivity.java
+++ app/src/main/java/proj/wifilocator/MainActivity.java
@@ -4,16 +4,86 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.io.IOException;
+import java.util.List;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.R.*;
+import com.google.android.gms.maps.model.Marker;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends ActionBarActivity {
+
+
+public class MainActivity extends FragmentActivity {
+
+    GoogleMap googleMap;
+    MarkerOptions markerOptions;
+    LatLng latLng;
+    static final LatLng TORONTO = new LatLng(43.653226, -79.383184);
+
+    private GoogleMap map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        MapFragment SMapFragment = (MapFragment)
+                getFragmentManager().findFragmentById(R.id.map);
+
+        // Getting a reference to the map
+        googleMap = SMapFragment.getMap();
+
+
+        // Getting reference to btn_find of the layout activity_main
+        Button btn_find = (Button) findViewById(R.id.btn_find);
+
+        // Defining button click event listener for the find button
+        OnClickListener findClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Getting reference to EditText to get the user input location
+                EditText etLocation = (EditText) findViewById(R.id.et_location);
+
+                // Getting user input location
+                String location = etLocation.getText().toString();
+
+                if (location != null && !location.equals(""))
+
+                {
+                    new GeocoderTask().execute(location);
+                }
+            }
+        };
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        Marker toronto = map.addMarker(new MarkerOptions().position(TORONTO)
+                .title("Toronto"));
+
+
+        btn_find.setOnClickListener(findClickListener);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,18 +92,57 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    // An AsyncTask class for accessing the GeoCoding Web Service
+    private class GeocoderTask extends AsyncTask<String, Void, List<Address>>{
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        protected List<Address> doInBackground(String... locationName) {
+            // Creating an instance of Geocoder class
+            Geocoder geocoder = new Geocoder(getBaseContext());
+            List<Address> addresses = null;
+
+            try {
+                // Getting a maximum of 3 Address that matches the input text
+                addresses = geocoder.getFromLocationName(locationName[0], 3);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return addresses;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(List<Address> addresses) {
+
+            if(addresses==null || addresses.size()==0){
+                Toast.makeText(getBaseContext(), "No Location found", Toast.LENGTH_SHORT).show();
+            }
+
+            // Clears all the existing markers on the map
+            googleMap.clear();
+
+            // Adding Markers on Google Map for each matching address
+            for(int i=0;i<addresses.size();i++){
+
+                Address address = (Address) addresses.get(i);
+
+                // Creating an instance of GeoPoint, to display in Google Map
+                latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                String addressText = String.format("%s, %s",
+                        address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
+                        address.getCountryName());
+
+                markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(addressText);
+
+                googleMap.addMarker(markerOptions);
+
+                // Locate the first location
+                if(i==0)
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        }
     }
 }
+
